ccd7e81165adae697ffa67988a566e26eaab3783
Luca DeCaprio <lucamatthewdecaprio@gmail.com>
2016-02-22 22:04:20 -0500

    Simple component

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function () {
+    'use strict';
+
+    var divStyle = {
+        color: 'blue'
+    };
+
+    var TestComponent = React.createClass({
+        displayName: 'TestComponent',
+
+
+        render: function render() {
+            return React.createElement(
+                'header',
+                { style: divStyle },
+                React.createElement(
+                    'h1',
+                    null,
+                    'My first React Component'
+                )
+            );
+        }
+    });
+    
+})();
